869687fbdbcd9db05a9cfc5d7438338fb29418ac
lehuutiendung <lehuutiendung972000@gmail.com>
2022-07-24 21:56:31 +0700

    Service RS 24/7

--- main.py
+++ main.py
@@ -18,26 +18,37 @@ client = MongoClient('localhost:27017')
 db = client.socialnetwork_recommendation_db
 
 if __name__ == "__main__":
-    # reacts = db.reacts.find()
-    # data_matrix = data_process_package.data_process.convert_data_toframe(dumps(reacts))
-    
-    data_matrix = data_process_package.data_process.get_data_from_json("G:/SocialNetwork_RecommendationSystems/RecommendWebAPI/dataset/view_data.json")
+    # 70% tập train
+    data_matrix_train = data_process_package.data_process.get_data_from_json("G:/SocialNetwork_RecommendationSystems/RecommendWebAPI/dataset/Train/view_data_train.json")
+    # 30% tập test
+    data_matrix_test = data_process_package.data_process.get_data_from_json("G:/SocialNetwork_RecommendationSystems/RecommendWebAPI/dataset/Test/view_data_test.json")
 
-    cf_result = recommender_package.collab_filtering_train.CollabFiltering(data_matrix, k=2, uuCF=1)
+    rate_train = np.asmatrix(data_matrix_train)
+    rate_test = np.asmatrix(data_matrix_test)
+
+    n_tests = rate_test.shape[0]
+
+    # User - user
+    cf_result = recommender_package.collab_filtering_train.CollabFiltering(data_matrix_train, k=2, uuCF=1)
     cf_result.fit()
 
-    rate_test = np.asmatrix(data_matrix)
-    n_tests = rate_test.shape[0]
-    SE = 0 # squared error
+    # # Item - item
+    # cf_result = recommender_package.collab_filtering_train.CollabFiltering(data_matrix_train, k=2, uuCF=0)
+    # cf_result.fit()
+
+    SE = 0 #error
+    SE_MAE = 0 #Absolute error
     for n in range(n_tests):
         pred = cf_result.pred(rate_test[n, 0], rate_test[n, 1], normalized = 0)
         SE += (pred - rate_test[n, 2])**2 
+        SE_MAE += (pred - rate_test[n, 2])
 
-    RMSE = np.sqrt(SE/n_tests)
-    print("User-user CF, RMSE =", RMSE)
-
-    list_result = cf_result.recommend_top(1, 10)
-    # print(list_result)
+    MAE = np.sqrt(SE_MAE/n_tests)
+    MSE = (SE/n_tests)
+    RMSE = np.sqrt(MSE)
+    print("CF, MAE =", MAE)
+    print("CF, MSE =", MSE)
+    print("CF, RMSE =", RMSE)
     
     # list_post_title = data_process_package.data_process.get_title_post("G:/SocialNetwork_RecommendationSystems/RecommendWebAPI/dataset/post_data.txt")
     # list_post_category = data_process_package.data_process.get_category_post("G:/SocialNetwork_RecommendationSystems/RecommendWebAPI/dataset/post_data.txt")
